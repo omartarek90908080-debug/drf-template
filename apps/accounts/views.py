10e0750a5455a0fd0472b4d3f2eb4f59c530bfb9
omartarek <omartarek90908080@gmail.com>
2026-04-19 14:43:06 +0200

    Addd New View

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,5 +1,9 @@
 from rest_framework import generics, status, permissions
 from .serializers import RegisterSerializer, LoginSerializer
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
 from .models import User
 
 # Create your views here.
@@ -9,6 +13,24 @@ class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
     queryset = User.objects.all()
     permission_classes = [permissions.AllowAny]
-    
-class LoginView(generics.)    
-    
+
+
+class LoginView(APIView):
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data["user"]
+
+        refresh = RefreshToken.for_user(user)
+
+        return Response(
+            {
+                "message": "Login Success",
+                "tokens": {
+                    "refresh": str(refresh),
+                    "access": str(refresh.access_token),
+                },
+                "user": {"id": user.id, "email": user.email},
+            },
+            status=status.HTTP_200_OK,
+        )
